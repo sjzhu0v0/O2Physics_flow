2267e1224e6d45ff07776d9e0f85566f3ccd3119
sjzhu <Senjie Zhu>
2026-03-12 16:11:17 +0800

    debug

--- PWGDQ/Tasks/tableFlow.cxx
+++ PWGDQ/Tasks/tableFlow.cxx
@@ -58,6 +58,8 @@ using namespace o2;
 using namespace o2::framework;
 using namespace o2::framework::expressions;
 using namespace o2::aod;
+using namespace o2::aod::rctsel;
+
 
 // Some definitions
 namespace o2::aod
